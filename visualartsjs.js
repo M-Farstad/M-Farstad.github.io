2acb4f9508f0fabb113a99bb63b6378e39e6090b
Marcus Farstad <mf608@cam.ac.uk>
2018-01-09 22:01:59 +0100

    first commit

---
+++ visualartsjs.js
@@ -0,0 +1,106 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+    "use strict";
+    $("#SodaFountain").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/SodaFountain/SodaFountain2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#SodaFountain").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    });
+    
+    $("#Falling").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/Falling/Falling2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#Falling").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    });
+    
+    $("#BigGulp").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/BigGulp/BigGulp2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#BigGulp").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    });
+    
+    $("#AllYouCanHave").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/AllYouCanHave/AllYouCanHave2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#AllYouCanHave").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    });
+    
+    $("#YouHaveItAll").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/YouHaveItAll/YouHaveItAll.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#AllYouCanHave").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    });
+    
+    $("#Decadence").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/Decadence/Decadence2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#Decadence").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    });
+    
+    
+    $("#ThatIsSoLastSeason").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/ThatIsSoLastSeason/ThatIsSoLastSeason2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#ThatIsSoLastSeason").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    }); 
+    
+        
+    $("#CokeBottle").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/CokeBottle/CokeBottle2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#CokeBottle").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    }); 
+
+            
+    $("#Conflict").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/Conflict/Conflict2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#Conflict").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    }); 
+    
+                
+    $("#Compassion").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/Compassion/Compassion2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#Compassion").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    }); 
+    
+    $("#Bjoernpistol").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/Bjoernpistol/Bjoernpistol2.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#Bjoernpistol").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    }); 
+    
+    $("#Providence").mouseenter(function () {
+        $(".background-image").css("background-image", "url(images/Providence/Providence.jpg)");
+        $(".background-image").css("background-size", "cover");
+    });
+    $("#Providence").mouseleave(function () {
+        $(".background-image").css("background", "none");
+    }); 
+
+
+});
